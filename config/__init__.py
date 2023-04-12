84acc300bed2e1e6ac751a13aca2c6de9ad0b916
webstarlee <webstar.lee@gmail.com>
2023-04-12 17:16:51 +0300

    pro env

--- config/__init__.py
+++ config/__init__.py
@@ -3,13 +3,13 @@ from pymongo import MongoClient
 mongo_url = "mongodb://mongo:0vAcYlPN44ugKCNqFXNc@containers-us-west-203.railway.app:6725"
 mongo_client = MongoClient(mongo_url)
 
-mongo_db = mongo_client['shill_test']
-bot_token = "5980518310:AAH59J2CU_roxuHRfArHzq3HTuB_Dlymp-4"
-leaderboard_id = "-1001894150735"
+# mongo_db = mongo_client['shill_test']
+# bot_token = "5980518310:AAH59J2CU_roxuHRfArHzq3HTuB_Dlymp-4"
+# leaderboard_id = "-1001894150735"
 
-# mongo_db = mongo_client['shillmaster']
-# bot_token = "6127801894:AAExOYxc_EHywwg664RmWGg2myVRThN9mL4"
-# leaderboard_id = "-1001964784230"
+mongo_db = mongo_client['shillmaster']
+bot_token = "6127801894:AAExOYxc_EHywwg664RmWGg2myVRThN9mL4"
+leaderboard_id = "-1001964784230"
 
 #coinmarketcap api
 cmc_key = "e8456919-e960-45bf-865c-96d04f767c7c"
