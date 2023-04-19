796285402e0a300ed465d683d7d00ab3d940b9b2
webstarlee <webstar.lee@gmail.com>
2023-04-20 04:43:20 +0900

    add dex routers

--- config/routers.py
+++ config/routers.py
@@ -1,3 +1,10 @@
 eth_routers={
-    "uniswap": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D "
+    "uniswap": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    "shibaswap": "0x03f7724180AA6b939894B5Ca4314783B0b36b329",
+    "sushiswap": "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+    "pancakeswap": "0xEfF92A263d31888d860bD50809A8D171709b7b1c"
+}
+
+bsc_routers={
+    "pancakeswap": "0x10ED43C718714eb63d5aA57B78B54704E256024E"
 }
